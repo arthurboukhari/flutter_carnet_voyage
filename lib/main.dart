fcee772b2c38430726ea14031f3b18ee348b7a1f
Jean-Marin <135821528+jmmnr49@users.noreply.github.com>
2023-07-04 10:11:16 +0200

    réparation fonctionnement app sur web et android

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -6,30 +8,39 @@ import 'package:flutter_carnet_voyage/repositories/user_repository.dart';
 import 'package:flutter_carnet_voyage/ui/screens/bottom_navigation.dart';
 import 'package:flutter_carnet_voyage/ui/screens/contact_detail_screen.dart';
 import 'package:flutter_carnet_voyage/ui/screens/login_screen.dart';
-
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'blocs/user_cubit.dart';
 
-main() async {
-  await Firebase.initializeApp(
+void main() async {
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
     options: FirebaseOptions(
       apiKey: "AIzaSyC8CXh5lxBfHH6-k3PuSl0DPD-PsWNd1DQ",
       appId: "1:83707367094:android:1f57908fef9ff90d08a017",
       messagingSenderId: "83707367094",
-      projectId: "flutter-carnet-de-voyage"
-    )
+      projectId: "flutter-carnet-de-voyage",
+    ),
   );
+  }
+  else {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  }
 
   final UserRepository userRepository = UserRepository(FirebaseAuth.instance);
   final UserCubit userCubit = UserCubit(userRepository);
   await userCubit.init();
+
   runApp(
     MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (_) => userCubit)
+          create: (_) => userCubit,
+        ),
       ],
-      child: const MyApp()
-    )
+      child: const MyApp(),
+    ),
   );
 }
 
